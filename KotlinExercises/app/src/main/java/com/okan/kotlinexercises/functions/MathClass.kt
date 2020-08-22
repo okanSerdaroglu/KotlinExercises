7f7b5f98c23f1e97529c2c99d420478dfd24dd39
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-08-22 19:47:21 +0300

    Math Class

---
+++ KotlinExercises/app/src/main/java/com/okan/kotlinexercises/functions/MathClass.kt
@@ -0,0 +1,21 @@
+package com.okan.kotlinexercises.functions
+
+import kotlin.math.abs
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.pow
+
+fun main() {
+
+    println(Math.E)
+    println(Math.PI)
+    println(abs(-5)) // Math.Abs
+    println(ceil(8.8))
+    println(floor(5.5))
+    println(2.2.pow(3.0))
+
+    println((Math.random() * 100).toInt()) // this method generates number between 0 and 1
+
+    
+
+}
