126d6f7565b235e14630f27f63f5ad9e3fe61033
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-08-22 22:30:27 +0300

    Arrays

---
+++ KotlinExercises/app/src/main/java/com/okan/kotlinexercises/functions/Arrays.kt
@@ -0,0 +1,16 @@
+package com.okan.kotlinexercises.functions
+
+fun main() {
+
+    var numbers = Array(10) { 0 }
+
+    var charArray = Array(4) { "" }
+
+    for (i in charArray.indices) { // this means i in 0..charArray.size -1
+        println(i)
+    }
+
+    var names = arrayOf("okan", true, 6) // any kind of variables
+
+
+}
