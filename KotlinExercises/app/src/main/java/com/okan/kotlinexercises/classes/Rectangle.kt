8c0cfe1b4f70418a4fa0eac60943be841cbd6b0d
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-08-23 15:10:51 +0300

    Function Parameters

---
+++ KotlinExercises/app/src/main/java/com/okan/kotlinexercises/classes/Rectangle.kt
@@ -0,0 +1,32 @@
+package com.okan.kotlinexercises.classes
+
+class Rectangle(lenght: Int = 0, height: Int = 0) {
+
+    fun calculateArea(): Int {
+        return lenght * height
+    }
+    // default values in constructor
+
+    private var lenght: Int
+    private var height: Int
+
+    init {
+        this.lenght = lenght
+        this.height = height
+    }
+
+}
+
+fun main() {
+
+    var rectangle = Rectangle() // default values 0
+    print(rectangle.calculateArea())
+
+    var rectangle1 = Rectangle(
+        lenght = 10,
+        height = 10
+    )
+
+    print()
+
+}
